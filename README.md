e204e25ce72a84970778ff7bc2e61556bde871f2
looput <lupu@hust.edu.cn>
2019-10-13 12:33:14 +0800

    1.1

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # pytorch_doc_build
 Build file of Pytorch docs
 ## How to use
-Clone this project to your computer, just use your browser open the `index.html`
+Clone this project to your computer, use your browser to open the `index.html`
 
 ## how to build
 ```bash
 git clone https://github.com/pytorch/pytorch.git
-cd pytorch/doc
+cd pytorch/docs
 pip install -r requirements.txt
 make html
 ```
--- _static/katex_autorenderer.js
+++ _static/katex_autorenderer.js
@@ -1,10 +1,9 @@
 katex_options = {
-delimiters : [
-   {left: "$$", right: "$$", display: true},
-   {left: "\\(", right: "\\)", display: false},
-   {left: "\\[", right: "\\]", display: true}
-],
 
+delimiters: [
+        { left: "\\(", right: "\\)", display: false },
+        { left: "\\[", right: "\\]", display: true }
+        ]
 }
 document.addEventListener("DOMContentLoaded", function() {
   renderMathInElement(document.body, katex_options);
